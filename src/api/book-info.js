20240aad867ddca6f31c30e3262660abd6f10c10
oomusou <oomusou@gmail.com>
2019-02-04 21:32:28 +0800

    Cache Promise

--- src/api/book-info.js
+++ src/api/book-info.js
@@ -3,4 +3,14 @@ import axios from 'axios';
 const API = process.env.VUE_APP_API;
 const URI = process.env.VUE_APP_BOOK_INFO;
 
-export default args => axios.get(`${API}${URI}`, { params: args });
+const toKey = args => Object.values(args).join('_');
+const fn = args => axios.get(`${API}${URI}`, { params: args });
+
+export default (() => {
+  const storage = {};
+
+  return args => {
+    const key = toKey(args);
+    return (storage[key]) ? storage[key] : storage[key] = fn(args);
+  };
+})();
